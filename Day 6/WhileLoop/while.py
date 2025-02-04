0372e3ded3207bcdbd16351c478d0497fa987b8e
Pepe Camarena <jjcamarena@gmail.com>
2025-02-04 01:25:58 -0600

    While Loop foundations

---
+++ Day 6/WhileLoop/while.py
@@ -0,0 +1,30 @@
+
+number_of_jumps = 6
+
+while number_of_jumps > 0:
+    print(number_of_jumps)
+    number_of_jumps -= 1
+
+# Reeborg's World
+# Hurdles 3
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+#
+# while not at_goal():
+#     if wall_in_front() == True:
+#         jump()
+#     else:
+#         move()
