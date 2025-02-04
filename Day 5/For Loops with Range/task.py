abcb8d52f9297bdb8e6fe83e5ef4234446da3cc7
Pepe Camarena <jjcamarena@gmail.com>
2025-02-03 22:56:39 -0600

    Password generator

---
+++ Day 5/For Loops with Range/task.py
@@ -0,0 +1,14 @@
+# Range Function with For Loop
+
+for number in range(1, 11):
+    print(number)
+
+for number in range(1, 11, 3):
+    print(number)
+
+suma = 0
+for number in range(1, 101):
+    suma += number
+    # suma = suma + number
+
+print (suma)
--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -1,10 +1,40 @@
+import random
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '@']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+nr_letters = int(input("How many letters would you like in your password? "))
+nr_symbols = int(input(f"How many symbols would you like? "))
+nr_numbers = int(input(f"How many numbers would you like? "))
 
+# #Eazy Level - Order not randomised:
+# #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+password = ""
+for i in range (0, nr_letters):
+    password += random.choice(letters)
 
+for i in range (0, nr_symbols):
+    password += random.choice(symbols)
+
+for i in range(0, nr_numbers):
+    password += random.choice(numbers)
+
+print("Easy: ", password)
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+tempPassword = password
+temp = ""
+hardPassword = ""
+print(len(password))
+for i in range(0, len(password) - 1):
+    # print(password)
+    temp = random.choice(password)
+    password = password.replace(temp, '')
+    # print(password)
+    hardPassword += temp
+
+print("Hard: ", hardPassword)
